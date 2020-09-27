3fc6c6867e05402f3eb1548bf6c2f235a7693c68
karlaromerox3 <kdaniela6@gmail.com>
2020-09-27 17:34:42 -0500

    viendo que onda

--- SetUpReact/my-app/src/App.js
+++ SetUpReact/my-app/src/App.js
@@ -7,11 +7,11 @@ class App extends Component{
     return (
       <div className="App">
         <div className="App-header">
-        <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
+        <nav className="navbar navbar-inverse">
+  <div className="container-fluid">
     <img src={logo} className="App-logo" alt="GAMECH logo" />
     <h1 className="title">GAMECH</h1>
-    <ul class="nav navbar-nav navbar-right">
+    <ul className="nav navbar-nav navbar-right">
       <li>
         <button type="button" className="btn btn-outline-primary" data-toggle="modal" data-target="#registrarse">Registrarse</button>
         &nbsp;
