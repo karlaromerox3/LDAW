c20367c7c522d511de211177e57ea5f2af8ae33d
angie_anaya <angianaya17@gmail.com>
2020-10-08 20:16:20 -0500

    Fixing the backend setup

---
+++ SetUpLaravel/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+use Illuminate\Database\Connection;
+
+Route::get('/', function () {
+
+    // Test database connection
+    try {
+        DB::connection()->getPdo();
+        echo "Connected successfully to: " . DB::connection()->getDatabaseName();
+    } catch (\Exception $e) {
+        die("Could not connect to the database. Please check your configuration. error:" . $e );
+    }
+
+    return view('welcome');
+});
